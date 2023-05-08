7f4b3054faa41a3ebe1bda97eaf929bbb8b92a6d
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-05-08 04:19:37 -0700

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Nicolai Jensen Gangdal on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
