0e33005dbb307eabc8586c783016b7093201357c
NicolaiJGangdal <nicolai.gangdal@hotmail.com>
2023-05-08 08:37:11 -0700

    Front-page first version + added images to assets directory

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Checking new branch")        }
-        .padding()
+        ZStack {
+            Image("workoutPhoto1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height:250)
+            
+            
+            //Vertical Stack - where objects are stacked vertically
+            VStack {
+                
+                Spacer()
+                
+                Text("Full Body Workout")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                Text("For Beginners")
+                    .fontWeight(.regular)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .frame(width:400)
+            .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+        }
+        .frame(width: 380, height: 300)
+        .cornerRadius(55)
+        .clipped()
+        .shadow(radius: 8)
+        .padding(.top, 20)
     }
 }
 
